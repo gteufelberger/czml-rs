6584198d1761482cb99f7dcb8f9da8365f83839d
Georg Teufelberger <georg.teufelberger@beyondgravity.com>
2025-03-28 15:05:54 +0100

    feat: Add support for billboard object

--- tests/test_basic_packets.rs
+++ tests/test_basic_packets.rs
@@ -14,6 +14,7 @@ fn test_just_document_packet() {
         version: Some("1.0".to_string()),
         availability: None,
         description: None,
+        billboard: None,
     };
 
     assert_eq!(actual, expected[0]);
