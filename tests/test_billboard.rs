94528d162528dec4bb6709c553e1469a123d24d8
Georg Teufelberger <georg.teufelberger@beyondgravity.com>
2025-03-28 15:09:56 +0100

    refactor: Change file ending to `.czml` for JSON files that contain CZML

--- tests/test_billboard.rs
+++ tests/test_billboard.rs
@@ -3,7 +3,7 @@ use std::path::Path;
 
 #[test]
 fn test_deserialize_iss_billboard_example() {
-    let path = Path::new("tests/data/billboard_iss.json");
+    let path = Path::new("tests/data/billboard_iss.czml");
     let json_str = fs::read_to_string(path).expect("Failed to read JSON file");
     let expected: Vec<czml::Packet> =
         serde_json::from_str(&json_str).expect("Failed to parse JSON");
