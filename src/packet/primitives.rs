305a775eadd85c736c84b79e58d1401babcda0d9
Georg Teufelberger <georg.teufelberger@beyondgravity.com>
2025-04-02 13:56:16 +0200

    refactor: Define rename strategy for `Origin` for entire struct

--- src/packet/primitives.rs
+++ src/packet/primitives.rs
@@ -2,11 +2,9 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
-#[serde(rename_all = "camelCase")]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
 #[serde(deny_unknown_fields)]
 pub enum Origin {
-    #[serde(rename = "CENTER")]
     Center,
-    #[serde(rename = "LEFT")]
     Left,
 }
