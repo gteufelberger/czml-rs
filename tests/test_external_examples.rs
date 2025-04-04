adfbd24c60b16d74ea5fe33295e2e6b36043e088
Georg Teufelberger <georg.teufelberger@beyondgravity.com>
2025-04-04 11:43:17 +0200

    test: Add test for `ClampToGround.czml` sample file from Cesium sandcastle by grabbing the file directly from GitHub

--- tests/test_external_examples.rs
+++ tests/test_external_examples.rs
@@ -3,6 +3,24 @@ use reqwest;
 use serde_json;
 
 #[test]
+fn test_deserialize_clamp_to_ground_example_from_github() {
+    let url =
+        "https://raw.githubusercontent.com/CesiumGS/cesium/refs/heads/main/Apps/SampleData/ClampToGround.czml";
+
+    // Download the CZML file
+    let response = reqwest::blocking::get(url).expect("Failed to fetch CZML file");
+    assert!(response.status().is_success(), "Failed to fetch CZML file");
+
+    let czml_text = response.text().expect("Failed to read response text");
+
+    // Deserialize
+    let packets: Vec<Packet> =
+        serde_json::from_str(&czml_text).expect("Failed to deserialize CZML");
+
+    println!("{:#?}", packets);
+}
+
+#[test]
 fn test_deserialize_vehicle_example_from_github() {
     let url =
         "https://raw.githubusercontent.com/CesiumGS/cesium/refs/heads/main/Apps/SampleData/Vehicle.czml";
