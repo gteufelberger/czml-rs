8856e64e1340a4ba84c198dc5fd39b16fa95ff49
Georg Teufelberger <georg.teufelberger@beyondgravity.com>
2025-04-04 16:00:15 +0200

    test: Add slightly modified `simple.czml` from Cesium sandcastle

---
+++ tests/test_simple_example.rs
@@ -0,0 +1,12 @@
+use std::fs;
+use std::path::Path;
+
+#[test]
+fn test_deserialize_modified_simple_example() {
+    let path = Path::new("tests/data/simple-modified.czml");
+    let json_str = fs::read_to_string(path).expect("Failed to read JSON file");
+    let expected: Vec<czml::Packet> =
+        serde_json::from_str(&json_str).expect("Failed to parse JSON");
+
+    println!("{:#?}", expected);
+}
