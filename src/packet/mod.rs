28b0ee560703f8708b5ccc9bad671033771a9b3c
Georg Teufelberger <georg.teufelberger@beyondgravity.com>
2025-04-04 14:42:18 +0200

    feat: Add support for `epoch` field in `orientation` object

--- src/packet/mod.rs
+++ src/packet/mod.rs
@@ -97,6 +97,8 @@ pub struct Model {
 #[serde(deny_unknown_fields)]
 pub struct Orientation {
     #[serde(skip_serializing_if = "Option::is_none")]
+    pub epoch: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub unit_quaternion: Option<[f64; 4]>,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub velocity_reference: Option<String>,
